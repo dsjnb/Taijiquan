ecb1419ee6bcddf7df1ff4d5d4233524a0776430
yanjunxiao <yanjx@163.com>
2019-04-27 22:27:51 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/quanfa/taijiquanApp.java
@@ -0,0 +1,11 @@
+package com.atguigu.quanfa;
+
+public class taijiquanApp {
+    public static void main(String[] args) {
+        System.out.println("taijiquan11111111111");
+        System.out.println("taijiquan11111111111");
+        System.out.println("taijiquan11111111111");
+        System.out.println("taijiquan11111111111");
+        System.out.println("taijiquan11111111111");
+    }
+}
